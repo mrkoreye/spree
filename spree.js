3cd2fd41bce1df7e8736cb68e505a7c3ace4f851
Korey Kassir <mrkoreye@gmail.com>
2014-02-18 08:19:59 -0800

    there is still a flicker in safari

--- spree.js
+++ spree.js
@@ -1,11 +1,11 @@
 if (Meteor.isClient) {
 	Template.main.events({
-		"click .hamburger": function(e) {
+		"tap .main, click .hamburger": function(e) {
 			e.stopPropagation();
 			$('.main').toggleClass('menu-open');
 			$('.menu').toggleClass('menu-open');
 		},
-		"click .main": function() {
+		"tap .main, click .main": function() {
 			var $main = $('.main');
 			if ($main.hasClass('menu-open')) {
 				$main.toggleClass('menu-open');
