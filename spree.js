3b88af4d4bdc64679c3e18c2ee8c679fc1ac8bbc
Korey Kassir <kkassir@popsugar.com>
2014-02-20 13:52:42 -0800

    not animating back in

--- spree.js
+++ spree.js
@@ -1,19 +1,90 @@
 if (Meteor.isClient) {
-	Template.main.events({
-		"tap .main, click .hamburger": function(e) {
-			e.stopPropagation();
-			// $('.main').toggleClass('menu-open');
-// 			$('.menu').toggleClass('menu-open');
-			move('.main').set('margin-left', 200).end();
-		},
-		"tap .main, click .main": function() {
-			var $main = $('.main');
-			if ($main.hasClass('menu-open')) {
-				$main.toggleClass('menu-open');
-				$('.menu').toggleClass('menu-open');
-			}
+	Meteor.startup(function() {
+		$('head').append("<script src='move.js'></script>");
+		
+		animateBlip = function() {
+			var moveBack = move('.hamburger-back')
+				.scale(1)
+				.set('background-color', 'white')
+				.duration(0)
+				.end();
+				
+			move('.hamburger-back')
+				.set('opacity', 1)
+				.duration(0)
+				.end();
+				
+			move('.hamburger-back')
+				.scale(3)
+				.set('opacity', 0)
+				.set('background-color', 'black')
+				.duration(2500)
+				.then(moveBack)
+				.end();
 		}
-	})
+		
+		animateBlip();
+		Meteor.setInterval(animateBlip, 3000);
+	});
+	
+	Template.main.events({
+		"tap .hamburgers, click .hamburgers": function(e) {
+			e.stopPropagation();
+			
+			var animateMenu = function() {
+				var delay = 0;
+				var items = $('.menu span');
+				$('.main').hide();
+				$('.menu').show();
+				
+				move('.menu')
+					.set('opacity', 1)
+					.duration(200)
+					.end();
+					
+				_.each(items, function(item) {
+					move(item)
+						.scale(0)
+						.duration(0)
+						.end();
+					
+					move(item)
+						.scale(1)
+						.duration(300)
+						.delay(delay)
+						.ease('in')
+						.end();
+					
+					delay += 50;
+				})
+				
+				move('.hamburger-menu')
+					.set('opacity', 0.8)
+					.duration(3000)
+					.end();
+			}
+			
+			move('.main')
+				.scale(3)
+				.set('opacity', 0)
+				.duration(500)
+				.then(animateMenu)
+				.end();
+		}
+	});
+	
+	Template.menu.events({
+		"tap .hamburger-menu, click .hamburger-menu": function() {
+			$('.menu').hide();
+			$('.main').css('display', 'block');
+		
+			move('.main')
+				.scale(1)
+				.set('opacity', 1)
+				.duration(500)
+				.end()
+		}
+	});
 }
 
 
